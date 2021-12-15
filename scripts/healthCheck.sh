131c6db052e4ea2c9ef349c4447bec6f974df5ad
lot8229 <lot8229@kakao.com>
2021-12-15 15:36:05 +0900

    주석처리

--- scripts/healthCheck.sh
+++ scripts/healthCheck.sh
@@ -7,7 +7,7 @@ do
   RESPONSE=$(curl -s http://localhost:8080/v1/uncomfortable)
 #  UP_COUNT=$(echo $RESPONSE | grep 'UP' | wc -l)
 
-  if [ $STATUSCODE -eq 200 ]
+  if [ $RESPONSE.statuscode -eq 200 ]
   then # $RESPONSE 가 status 200 인지 확인
       echo "> Health check 성공"
       break
